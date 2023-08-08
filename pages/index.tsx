75704703870babf0460f822347b32b796105034b
kravets.taras.react <kravets.taras.react@gmail.com>
2023-08-08 13:02:26 +0300

    feat: added fetching from db

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,75 @@
-import { Inter } from "@next/font/google";
+import { Fragment } from "react";
 import RestaurantCard from "../components/home/RestaurantCard";
 import HomeHeader from "../components/home/HomeHeader";
+import { PrismaClient, PRICE, Cuisine, Location } from "@prisma/client";
 
-const inter = Inter({ subsets: ["latin"] });
+const prisma = new PrismaClient();
 
-export default function Home() {
+export interface RestaurantCardType {
+  id: number;
+  name: string;
+  main_image: string;
+  cuisine: string;
+  location: string;
+  price: string;
+  slug: string;
+}
+
+const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
+  const restaurants = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      cuisine: {
+        select: {
+          name: true,
+        },
+      },
+      slug: true,
+      location: {
+        select: {
+          name: true,
+        },
+      },
+      price: true,
+    },
+  });
+
+  const formattedRestaurants = restaurants?.map((restaurant) => ({
+    ...restaurant,
+    cuisine: restaurant.cuisine.name,
+    location: restaurant.location.name,
+  }));
+
+  return formattedRestaurants;
+};
+
+export default function Home({
+  restaurants,
+}: {
+  restaurants: RestaurantCardType[];
+}) {
   return (
-        <main>
-          <HomeHeader />
-          {/* CARDS */}
-          <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-            <RestaurantCard />
-          </div>
-        </main>
+    <main>
+      <HomeHeader />
+      <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+        {restaurants.length > 0 &&
+          restaurants.map((item) => (
+            <Fragment key={item.id}>
+              <RestaurantCard {...item} />
+            </Fragment>
+          ))}
+      </div>
+    </main>
   );
+}
+
+export async function getServerSideProps() {
+  const restaurants = await fetchRestaurants();
+
+  console.log(restaurants);
+  return {
+    props: { restaurants },
+  };
 }
